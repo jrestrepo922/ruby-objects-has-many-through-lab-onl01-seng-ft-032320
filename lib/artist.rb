43050a95b85ff26f748058627e6edfe41ccf0e95
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-08 19:28:40 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,28 @@
+require "pry"
+class Artist
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+    Song.all.select{|song| song.artist == self}
+  end
+
+  def new_song(song_name, genre)
+    Song.new(song_name, self, genre)
+  end
+
+  def  genres
+    self.songs.collect {|song| song.genre}
+    #binding.pry
+  end
+end
